0a652a4b64e794a8f4a357bd769f5509f0221ff9
Dom Brassey <dombrassey@gmail.com>
2016-09-15 12:01:10 -0700

    Begin rspec tests for user auth dxb

--- spec/features/registering_user.rb
+++ spec/features/registering_user.rb
@@ -1,17 +1,19 @@
-describe "Sign up" do
+require "rails_helper"
+
+feature "registering a user" do
   before do
-    visit root_path
+    visit '/'
     click_link "Sign up"
     fill_in "Email",    with: "user@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
   end
 
-  it "creates a user" do
-    expect {
-      click_button "Sign up"
-    }.to change(User, :count).by(1)
-  end
+  # it "creates a user" do
+  #   expect {
+  #     click_button "Sign up"
+  #   }.to change(User, :count).by(1)
+  # end
 
   it "becomes signed in" do
     click_button "Sign up"
